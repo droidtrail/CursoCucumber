fc7e9a5e566a5d55948c2cca07152e32b596ad06
Leandro Pereira <leandro.nares@gmail.com>
2018-07-21 22:06:28 -0300

    Reaproveitando cenários - REGEX. Seção 2, aula 12.

--- CursoCucumber/src/test/java/AprenderCucumber.java
+++ CursoCucumber/src/test/java/AprenderCucumber.java
@@ -1,3 +1,8 @@
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 import org.junit.Assert;
 
 import cucumber.api.java.pt.Dado;
@@ -42,6 +47,44 @@ public class AprenderCucumber {
 	   
 		Assert.assertEquals(arg1, contador);
 		
+	}
+	
+	Date entrega = new Date();
+	
+	@Dado("^que a entrega é dia (\\d+)/(\\d+)/(\\d+)$")
+	public void queAEntregaÉDia(int dia, int mes, int ano) throws Throwable {
+		
+		Calendar cal = Calendar.getInstance();
+		cal.set(Calendar.DAY_OF_MONTH, dia);
+		cal.set(Calendar.MONTH, mes -1);
+		cal.set(Calendar.YEAR, ano);
+		entrega = cal.getTime();
+		
+	    
+	}
+
+	@Quando("^a entrega atrasar em (\\d+) (dia|dias|mes|meses)$")
+	public void aEntregaAtrasarEmDias(int arg1, String tempo) throws Throwable {
+		
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(entrega);
+		
+		if(tempo.equals("dias")) {
+			cal.add(Calendar.DAY_OF_MONTH, arg1);
+		}
+		if(tempo.equals("meses")) {
+			cal.add(Calendar.MONTH, arg1);
+		}
+		entrega = cal.getTime();  
+	}
+
+	@Então("^a entrega será efetuada em (\\d{2}\\/\\d{2}\\/\\d{4})$")
+	public void aEntregaSeráEfetuadaEm(String data) throws Throwable {
+		
+		DateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+		String dataFormatada = format.format(entrega);
+		Assert.assertEquals(data, dataFormatada);
+	    
 	}
 
 
